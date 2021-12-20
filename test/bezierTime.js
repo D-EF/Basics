ba8769302ee20f9cc36c1c182c235fa7eae52e36
DEF <darth_ef@hotmail.com>
2021-12-20 15:52:30 +0800

    修正计算错误

--- test/bezierTime.js
+++ test/bezierTime.js
@@ -6,62 +6,69 @@ var t=0;
 //此处修改曲线精度
 var tspLength=0.001;
 //此处修改数据长度
-var d_length=100000;
+var d_length=1000;
 
 
 
-var d=new Array(d_length);
-for(var i =d.length-1;i>=0;--i){
-    d[i]=[
+var dde=new Array(d_length);
+for(var i =dde.length-1;i>=0;--i){
+    dde[i]=[
         {x:Math.random()*100,y:Math.random()*100},
         {x:Math.random()*100,y:Math.random()*100},
         {x:Math.random()*100,y:Math.random()*100},
         {x:Math.random()*100,y:Math.random()*100}
     ];
 }
-var kpt2=[];
+
+var d3=new Array(d_length);
+for(var i =d3.length-1;i>=0;--i){
+    d3[i]={
+        p1:{x:dde[i][0].x,y:dde[i][0].y},
+        p2:{x:dde[i][1].x,y:dde[i][1].y},
+        p3:{x:dde[i][2].x,y:dde[i][2].y},
+        p4:{x:dde[i][3].x,y:dde[i][3].y}
+    }
+}
+
+var ktp2=[];
 var nt=performance.now();
-for(var i =d.length-1;i>=0;--i){
+for(var i =dde.length-1;i>=0;--i){
     for(t=0;t<=1;t+=tspLength){
-        // kpt2.push
+        // ktp2.push
         (
-        getBezierCurvePoint(d[i],t)
-    )
+            getBezierCurvePoint(dde[i],t)
+        )
     }
 }
-console.log(kpt2);
-console.log("使用时间",performance.now()-nt);
+console.log(ktp2);
+console.log("de Casteljau's algorithm 使用时间",performance.now()-nt);
 
 
 
-var d=new Array(d_length);
-for(var i =d.length-1;i>=0;--i){
-    d[i]={
-        p1:{x:Math.random()*100,y:Math.random()*100},
-        p2:{x:Math.random()*100,y:Math.random()*100},
-        p3:{x:Math.random()*100,y:Math.random()*100},
-        p4:{x:Math.random()*100,y:Math.random()*100}
-    }
-}
 var nt=performance.now();
 var ktp1=[];
 var sx,sy;
-for(var i =d.length-1;i>=0;--i){
-    sx=d[i].p4.x-d[i].p1.x;
-    sy=d[i].p4.y-d[i].p1.y;
+for(var i =d3.length-1;i>=0;--i){
+    sx=d3[i].p4.x-d3[i].p1.x;
+    sy=d3[i].p4.y-d3[i].p1.y;
+    var sx_i=1/sx,
+        sy_i=1/sy;
     var p2x,p2y,p3x,p3y;
-    p2x=d[i].p2.x*sx;
-    p2y=d[i].p2.y*sy;
-    p3y=d[i].p3.y*sy;
-    p3x=d[i].p3.x*sx;
+    p2x=(d3[i].p2.x-d3[i].p1.x)*sx_i;
+    p2y=(d3[i].p2.y-d3[i].p1.y)*sy_i;
+    p3y=(d3[i].p3.y-d3[i].p1.y)*sy_i;
+    p3x=(d3[i].p3.x-d3[i].p1.x)*sx_i;
     var bezierObj=new UnitBezier(p2x,p2y,p3x,p3y);
     for(t=0;t<=1;t+=tspLength){
         // ktp1.push
         ({
-            x:bezierObj.sampleCurveX(t),
-            y:bezierObj.sampleCurveY(t)
+            x:bezierObj.sampleCurveX(t)*sx+d3[i].p1.x,
+            y:bezierObj.sampleCurveY(t)*sy+d3[i].p1.y
         });
     }
 }
 console.log(ktp1);
-console.log("使用时间",performance.now()-nt);
+console.log("三次函数 使用时间",performance.now()-nt);
+
+var i=1
+console.log(ktp1[i],ktp2[i])
